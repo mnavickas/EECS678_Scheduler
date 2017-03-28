30c882d97b814a2f898f0fbf615f17eb8faeb6da
Michael Navickas <mnavickas14@gmail.com>
2017-03-28 15:54:07 -0500

    Start up

--- scheduler/src/libscheduler/libscheduler.c
+++ scheduler/src/libscheduler/libscheduler.c
@@ -11,7 +11,7 @@
 
 /**
   Stores information making up a job to be scheduled including any statistics.
-  You may need to define some global variables or a struct to store your job queue elements. 
+  You may need to define some global variables or a struct to store your job queue elements.
 */
 typedef struct _job_t
 {
@@ -49,7 +49,7 @@ int SJFcompare(const void *a, const void *b)
 {
   job_t const *left = (job_t*)a;
   job_t const *right = (job_t*)b;
-  return left->total_time_needed - right->total_time_needed; 
+  return left->total_time_needed - right->total_time_needed;
 }
 
 int PRIcompare(const void *a, const void *b)
@@ -59,18 +59,18 @@ int PRIcompare(const void *a, const void *b)
 
   if( left->priority == right->priority)
   {
-    return (right->arrival_time - left->arrival_time) * -1; 
+    return (right->arrival_time - left->arrival_time) * -1;
   }
   else
   {
-    return (right->priority - left->priority) * -1; 
+    return (right->priority - left->priority) * -1;
   }
-  
+
 }
 
 /**
   Initalizes the scheduler.
- 
+
   Assumptions:
     - You may assume this will be the first scheduler function called.
     - You may assume this function will be called once once.
@@ -82,13 +82,31 @@ int PRIcompare(const void *a, const void *b)
 */
 void scheduler_start_up(int cores, scheme_t scheme)
 {
+    scheduler_ptr = (scheduler_t *) calloc( 1, sizeof(scheduler_t) );
+	scheduler_ptr->core_count = cores;
+	scheduler_ptr->current_jobs_on_cores = (job_t **) calloc( cores , sizeof(job_t*) );
 
+	switch(scheme)
+	{
+		case FCFS:
+		case RR:
+			priqueue_init(&scheduler_ptr->job_queue, FCFScompare);
+			break;
+		case SJF:
+		case PSJF:
+			priqueue_init(&scheduler_ptr->job_queue, SJFcompare);
+			break;
+		case PRI:
+		case PPRI:
+			priqueue_init(&scheduler_ptr->job_queue, PRIcompare);
+			break;
+	}
 }
 
 
 /**
   Called when a new job arrives.
- 
+
   If multiple cores are idle, the job should be assigned to the core with the
   lowest id.
   If the job arriving should be scheduled to run during the next
@@ -103,8 +121,8 @@ void scheduler_start_up(int cores, scheme_t scheme)
   @param running_time the total number of time units this job will run before it will be finished.
   @param priority the priority of the job. (The lower the value, the higher the priority.)
   @return index of core job should be scheduled on
-  @return -1 if no scheduling changes should be made. 
- 
+  @return -1 if no scheduling changes should be made.
+
  */
 int scheduler_new_job(int job_number, int time, int running_time, int priority)
 {
@@ -114,12 +132,12 @@ int scheduler_new_job(int job_number, int time, int running_time, int priority)
 
 /**
   Called when a job has completed execution.
- 
+
   The core_id, job_number and time parameters are provided for convenience. You may be able to calculate the values with your own data structure.
   If any job should be scheduled to run on the core free'd up by the
   finished job, return the job_number of the job that should be scheduled to
   run on core core_id.
- 
+
   @param core_id the zero-based index of the core where the job was located.
   @param job_number a globally unique identification number of the job.
   @param time the current time of the simulator.
@@ -135,13 +153,13 @@ int scheduler_job_finished(int core_id, int job_number, int time)
 /**
   When the scheme is set to RR, called when the quantum timer has expired
   on a core.
- 
+
   If any job should be scheduled to run on the core free'd up by
   the quantum expiration, return the job_number of the job that should be
   scheduled to run on core core_id.
 
   @param core_id the zero-based index of the core where the quantum has expired.
-  @param time the current time of the simulator. 
+  @param time the current time of the simulator.
   @return job_number of the job that should be scheduled on core cord_id
   @return -1 if core should remain idle
  */
@@ -192,7 +210,7 @@ float scheduler_average_response_time()
 
 /**
   Free any memory associated with your scheduler.
- 
+
   Assumptions:
     - This function will be the last function called in your library.
 */
@@ -208,8 +226,8 @@ void scheduler_clean_up()
   makes to your scheduler.
   In our provided output, we have implemented this function to list the jobs in the order they are to be scheduled. Furthermore, we have also listed the current state of the job (either running on a given core or idle). For example, if we have a non-preemptive algorithm and job(id=4) has began running, job(id=2) arrives with a higher priority, and job(id=1) arrives with a lower priority, the output in our sample output will be:
 
-    2(-1) 4(0) 1(-1)  
-  
+    2(-1) 4(0) 1(-1)
+
   This function is not required and will not be graded. You may leave it
   blank if you do not find it useful.
  */
